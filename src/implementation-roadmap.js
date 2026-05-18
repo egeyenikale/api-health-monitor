ee47ef993adda02272982524aec24196dae28ef0
Ege Hidayet Koca <kocaege777@gmail.com>
2026-05-18 09:56:00 +0300

    ci: add build and test workflow

--- src/implementation-roadmap.js
+++ src/implementation-roadmap.js
@@ -33,3 +33,8 @@ export const stage7 = {
   "date": "2026-05-04",
   "message": "docs: add installation and usage documentation"
 };
+
+export const stage8 = {
+  "date": "2026-05-18",
+  "message": "ci: add build and test workflow"
+};
